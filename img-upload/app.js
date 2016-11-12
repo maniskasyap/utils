2c9447b2603dd9cfc008e7e824e0be0828c346d3
leanowl <manis.kasyap@gmail.com>
2016-11-12 19:39:08 +0530

    progress spinner

--- img-upload/app.js
+++ img-upload/app.js
@@ -1,8 +1,10 @@
 angular.module('photo-upload', [])
     .controller('uploadCtrl', ['$scope', '$http', function($scope, $http) {
         $scope.uploadPhoto = function() {
+        	$scope.uploading = true;
             $http.post('upload').then(function(response) {
-            	$scope.uploadRes = response;
+                $scope.uploadRes = response;
+                $scope.uploading = false;
             });
         };
     }]);
