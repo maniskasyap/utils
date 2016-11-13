4f5008e11b436354e5854d591bd3fc2ac3540190
leanowl <manis.kasyap@gmail.com>
2016-11-14 00:40:59 +0530

    form data

--- img-upload/server.js
+++ img-upload/server.js
@@ -1,8 +1,12 @@
 var express = require('express');
+var bodyParser = require("body-parser");
 var app = express();
 var path = require('path');
 var fs = require('fs');
 var cloudinary = require('cloudinary');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 // var cloud_name = 'leanowl';
 // var base_url = 'http://api.cloudinary.com/api/v1_1/' + cloud_name;
@@ -17,7 +21,7 @@ cloudinary.config({
     api_key: '174497384943137',
     api_secret: 'j73C5gjof7bXaygyZjM8cXPB_9o'
 });
-var img = "http://www.logospike.com/wp-content/uploads/2015/11/Logo_Image_03.png";
+// var img = "http://www.logospike.com/wp-content/uploads/2015/11/Logo_Image_03.png";
 
 app.use(express.static(path.join(__dirname)));
 
@@ -26,7 +30,8 @@ app.get('/', function(req, res) {
 });
 
 app.post('/upload', function(req, res) {
-    cloudinary.uploader.upload(img, function(result) {
+    console.log(req.body);
+    cloudinary.uploader.upload(req.body, function(result) {
         res.send(result);
     });
 });
