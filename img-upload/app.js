6d79730e340ba630521c91f914be7c9b9c2cb5a3
Manish Kumar-XFT <Manish Kumar-XFT@MCKINSEY.COM>
2017-01-25 12:51:05 +0530

    Revert "changes"
    This reverts commit ea6ab93dfe202316bb1a6b65e2dd091ddb4039a3.

--- img-upload/app.js
+++ img-upload/app.js
@@ -1,7 +1,7 @@
 angular.module('photo-upload', [])
     .controller('uploadCtrl', ['$scope', '$http', function($scope, $http) {
         $scope.uploadPhoto = function() {
-            $scope.uploading = true;
+        	$scope.uploading = true;
             $http.post('upload').then(function(response) {
                 $scope.uploadRes = response.data;
                 $scope.uploading = false;
@@ -9,23 +9,6 @@ angular.module('photo-upload', [])
         };
 
         $scope.getFile = function(event) {
-            $scope.file = event;
+        	$scope.file = event;
         };
-
-        $scope.upload = function(url, formData) {
-            var defer = $q.defer();
-
-            $http.post(server + url, formData, {
-                headers: { 'Content-Type': undefined }
-            }).then(
-                function(response) {
-                    return defer.resolve(response.data);
-                },
-                function(err) {
-                    return defer.reject(err);
-                }
-            );
-
-            return defer.promise;
-        }
     }]);
