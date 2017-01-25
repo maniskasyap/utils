0205c93c04bb0f2a6a7e76303bacbc9b2a1af6ed
Manish Kumar-XFT <Manish Kumar-XFT@MCKINSEY.COM>
2017-01-25 12:47:32 +0530

    Revert "form data"
    This reverts commit 4f5008e11b436354e5854d591bd3fc2ac3540190.

--- img-upload/app.js
+++ img-upload/app.js
@@ -1,41 +1,31 @@
 angular.module('photo-upload', [])
-    .directive('ngFiles', ['$parse', function($parse) {
-
-        function fn_link(scope, element, attrs) {
-            var onChange = $parse(attrs.ngFiles);
-            element.on('change', function(event) {
-                onChange(scope, { $files: event.target.files });
-            });
-        };
-
-        return {
-            link: fn_link
-        }
-    }])
     .controller('uploadCtrl', ['$scope', '$http', function($scope, $http) {
         $scope.uploadPhoto = function() {
-            var request = {
-                method: 'POST',
-                url: 'upload',
-                data: formdata,
-                headers: {
-                    'Content-Type': undefined
-                }
-            };
             $scope.uploading = true;
-            $http(request).success(function(response) {
-                    $scope.uploadRes = response;
-                    $scope.uploading = false;
-                })
-                .error(function(err) {
-                    alert(error);
-                });
-        };
-
-        var formdata = new FormData();
-        $scope.getTheFiles = function($files) {
-            angular.forEach($files, function(value, key) {
-                formdata.append(key, value);
+            $http.post('upload').then(function(response) {
+                $scope.uploadRes = response.data;
+                $scope.uploading = false;
             });
         };
+
+        $scope.getFile = function(event) {
+            $scope.file = event;
+        };
+
+        $scope.upload = function(url, formData) {
+            var defer = $q.defer();
+
+            $http.post(server + url, formData, {
+                headers: { 'Content-Type': undefined }
+            }).then(
+                function(response) {
+                    return defer.resolve(response.data);
+                },
+                function(err) {
+                    return defer.reject(err);
+                }
+            );
+
+            return defer.promise;
+        }
     }]);
